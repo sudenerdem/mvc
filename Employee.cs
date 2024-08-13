ca99f0e29c32beaa47a6651eb93af6f2cdf166c9
sudenerdem <sudenure482@gmail.com>
2024-08-13 13:57:00 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+namespace Basics.Model {
+ public class Employee {
+    public int Id {get; set; } 
+    public String FirstName{get; set; }= String.Empty;
+    public String LastName{get; set; }=String.Empty;
+    public String FullName => $"{FirstName} {LastName.ToUpper()}";
+       public int Age {get; set; } 
+
+ }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Net.Security;
+using Microsoft.AspNetCore.Mvc;
+using Basics.Model;
+
+
+
+
+
+namespace Basics.Controllers {
+public class EmployeeController :Controller
+{
+    public IActionResult Index1()
+    {
+        string message = $"Hello World {DateTime.Now.ToString()}";
+        return View("Index1",message);
+    }
+    public ViewResult Index2()
+    {
+        var names = new String[]
+        {  
+            "Ahmet",
+            "Mehmet",
+            "Can"
+
+        };
+        return View(names);
+    }
+    public IActionResult Index3()
+    {
+        var list= new List<Employee>()
+        {
+           new Employee(){Id=1, FirstName="Ahmet",LastName="Can",Age=20},
+           new Employee(){Id=2, FirstName="Can",LastName="Dağ",Age=25},
+           new Employee(){Id=3, FirstName="Demir",LastName="Güneş",Age=28},
+        };
+        return View("Index3",list);
+    }
+}
+}
